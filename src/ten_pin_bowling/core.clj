fdb19f8b5abc48ae7a63a0bfdc5fcf6cf53b9dcc
Tharaka Manawardhana <tharaka@infoyoda.com>
2018-11-19 01:47:12 +1100

    - Code restructuring/refactoring - Validation improved - Tests enhanced

--- src/ten_pin_bowling/core.clj
+++ src/ten_pin_bowling/core.clj
@@ -1,20 +1,41 @@
 (ns ten-pin-bowling.core)
 
-:strike ;all 10 pins knoked in one shot
-:spare ;all 10 pins knoked in 2 shots
-:open ; after the two attempts, some pins remains
+(defn start-game
+  "Creates an empty score card."
+  []
+  {:score 0
+   :frames []
+   :completed false})
 
+(defn valid-frame?
+  "When frame points (integer vector) frame position is given,
+  this returns true if valid and false otherwise"
+  [[p1 p2 p3 :as frame] f-id]
+  {:pre [(every? integer? frame)]}
+  (and
+   (every? identity (mapcat (juxt (complement nil?)
+                                  (complement neg?)
+                                  #(>= 10 %))
+                            frame))
+   (case (count frame)
+     1 (= p1 10)
+     2 (< p1 10)
+     3 (and (= 9 f-id)
+            (>= (+ p1 p2) 10))
+     false)))
 
-(defn frame-score [frames frame-id]
-  (let [frame-idx (dec frame-id)
-        frame (nth frames frame-idx nil)
+(defn frame-score
+  "Calculates the score of a single frame at f-id.
+  Due to the scoring rules, other frames are also needed"
+  [frames f-id]
+  (let [frame (nth frames f-id nil)
 
         ;;frame bawls
-        fbs (nth frames frame-idx nil)
+        fbs (nth frames f-id nil)
         [fb1 fb2 fb3] fbs
 
         ;;rounds ahead the frame (current frame inclusive)
-        rs (flatten (subvec frames frame-idx))
+        rs (flatten (subvec frames f-id))
         [r1 r2 r3] rs
 
         not-nil? (complement nil?)]
@@ -30,46 +51,22 @@
 
       :default 0)))
 
-
-
-(defn start-game
-  "Creates an empty score card."
-  []
-  {:score 0
-   :frames []
-   :completed false})
-
-
-(defn valid-frame? [[p1 p2 p3 :as frame] f-id]
-  (and
-   (every? identity
-           (mapcat (juxt (complement nil?)
-                         (complement neg?)
-                         integer?
-                         #(>= 10 %))
-                   frame))
-   (case (count frame)
-     1 (= p1 10)
-     2 (< p1 10)
-     3 (and (= 10 f-id)
-            (>= (+ p1 p2) 10))
-     (do (print ">>>>>" f-id) false))))
-
-
 (defn score
   "Frame succession funciton:
-  When incomplete score card and next frame rounds' points given
+  When incomplete score card and new frame points given
   it returns the score card after the given frame applied."
-  ([card [[p1 p2 p3] :as frame]]
-   {:pre [(valid-frame? frame (-> frame count inc))]}
+  ([card [p1 p2 p3 :as frame]]
+   {:pre [(valid-frame? frame (-> frame count dec))]}
    (score (update card :frames conj frame)))
+
   ([card]
    {:pre [(every? identity (map #(apply valid-frame? %)
-                                (zipmap (:frames card) (range 1 (-> (:frames card) count inc)))))]}
+                                (zipmap (:frames card)
+                                        (range (-> card :frames count)))))]}
 
    (let [frames (card :frames)
          frame-scores (map #(frame-score frames %)
-                           (range 1 (inc (count frames))))]
+                           (range (count frames)))]
      (-> card
          (assoc :score (reduce + frame-scores))
          (assoc :completed (= (count frames) 10))))))
--- test/ten_pin_bowling/core_test.clj
+++ test/ten_pin_bowling/core_test.clj
@@ -3,92 +3,75 @@
             [ten-pin-bowling.core :refer :all]))
 
 (deftest frame-score-test
-  (let [
-        card1 [#_1 [0 0]
-               #_2 [5 3]
-               #_3 [4 3]
-               #_4 [10 0]
-               #_5 [7 3]
-               #_6 [0 10]
-               #_7 [4 6]
-               #_8 [10 10]
-               #_9 [10 10]
-               #_10 [1 9 5]]
+  (let [;;see https://www.youtube.com/watch?v=aBe71sD8o8c
 
-        card2 [#_1 [8 2]
-               #_2 [7 3]
-               #_3 [3 4]
-               #_4 [10]
-               #_5 [2 8]
-               #_6 [10]
-               #_7 [10]
-               #_8 [8 0]
-               #_9 [10]
-               #_10 [8 2 9]]
+        card1 (start-game)
+        card1-1 (score card1 [2 3])
 
-        #_card2 #_ [#_1 [8 2]
-               #_2 [7 3]
-               #_3 [3 4]
-               #_4 [10]
-               #_5 [2 8]
-               #_6 [10]
-               #_7 [10]
-               #_8 [8 0]
-               #_9 [10]
-               #_10 [8 2 9]]
-
-        card3 [#_1 [0 0]
-               #_2 [5 3]
-               #_3 [4 4]
-               #_4 [10 0]
-               #_5 [7 3]
-               #_6 [0 10]
-               #_7 [4 6]
-               #_8 [10 10]
-               #_9 [10 10]
-               #_10 [10 10 10]]
+        frame-set-1 [#_1 [8 2]
+                     #_2 [7 3]
+                     #_3 [3 4]
+                     #_4 [10]
+                     #_5 [2 8]
+                     #_6 [10]
+                     #_7 [10]
+                     #_8 [8 0]
+                     #_9 [10]
+                     #_10 [8 2 9]]
 
         ;;http://web.cs.wpi.edu/~heineman/html/teaching_/2009_Fall_CS509/Examples/Oct-27/10-27.html
-        card4 [#_1 [1 4]
-               #_2 [4 5]
-               #_3 [6 4]
-               #_4 [5 5]
-               #_5 [10]
-               #_6 [0 1]
-               #_7 [7 3]
-               #_8 [6 4]
-               #_9 [10]
-               #_10 [2 8 6]]
+        frame-set-2 [#_1 [1 4]
+                     #_2 [4 5]
+                     #_3 [6 4]
+                     #_4 [5 5]
+                     #_5 [10]
+                     #_6 [0 1]
+                     #_7 [7 3]
+                     #_8 [6 4]
+                     #_9 [10]
+                     #_10 [2 8 6]]
 
         ;;http://www.tenpin.org.au/index.php?id=875
-        card5 [#_1 [10]
-               #_2 [9 1]
-               #_3 [5 5]
-               #_4 [7 2]
-               #_5 [10]
-               #_6 [10]
-               #_7 [10]
-               #_8 [9 0]
-               #_9 [8 2]
-               #_10 [9 1 10]]]
-
-  
+        frame-set-3 [#_1 [10]
+                     #_2 [9 1]
+                     #_3 [5 5]
+                     #_4 [7 2]
+                     #_5 [10]
+                     #_6 [10]
+                     #_7 [10]
+                     #_8 [9 0]
+                     #_9 [8 2]
+                     #_10 [9 1 10]]]
 
     (testing "Card 2 each frame scores"
-      (is (= 17 (frame-score card2 1)))
-      (is (= 13 (frame-score card2 2)))
-      (is (= 7 (frame-score card2 3)))
-      (is (= 20 (frame-score card2 4)))
-      (is (= 20 (frame-score card2 5)))
-      (is (= 28 (frame-score card2 6)))
-      (is (= 18 (frame-score card2 7)))
-      (is (= 8 (frame-score card2 8)))
-      (is (= 20 (frame-score card2 9)))
-      (is (= 19 (frame-score card2 10))))
+      (is (= 17 (frame-score frame-set-1 0)))
+      (is (= 13 (frame-score frame-set-1 1)))
+      (is (= 7 (frame-score frame-set-1 2)))
+      (is (= 20 (frame-score frame-set-1 3)))
+      (is (= 20 (frame-score frame-set-1 4)))
+      (is (= 28 (frame-score frame-set-1 5)))
+      (is (= 18 (frame-score frame-set-1 6)))
+      (is (= 8 (frame-score frame-set-1 7)))
+      (is (= 20 (frame-score frame-set-1 8)))
+      (is (= 19 (frame-score frame-set-1 9))))
 
     (testing "Total game score"
-      (is (= 170 (:score (score {:frames card2}))))
-      (is (= 133 (:score (score {:frames card4}))))
-      (is (= 187 (:score (score {:frames card5})))))))
+      (is (= 170 (:score (score {:frames frame-set-1}))))
+      (is (= 133 (:score (score {:frames frame-set-2}))))
+      (is (= 187 (:score (score {:frames frame-set-3})))))
+
+    (testing "partial state score"
+      (is (= 5 (:score (score card1-1))))
+      (is (= 0 (:score (score card1)))))
+
+    (testing "completeness"
+      (is (= false (:completed (score card1))))
+      (is (= false (:completed (score card1-1))))
+
+      (is (= true (:completed (score {:frames frame-set-1}))))
+      (is (= true (:completed (score {:frames frame-set-2}))))
+      (is (= true (:completed (score {:frames frame-set-3})))))))
+
+
 
 
